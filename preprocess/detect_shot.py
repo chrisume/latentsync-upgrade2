5eee4419d1002468f5d559943e6ee7f81d9837d4
unknown <pedro.mslabonia@gmail.com>
2025-03-24 00:29:17 -0300

    fix: ignore extra data tracks such as timecode tracks

--- preprocess/detect_shot.py
+++ preprocess/detect_shot.py
@@ -29,13 +29,15 @@ def gather_paths(input_dir, output_dir):
                 continue
             paths.append([video_input, output_dir])
         elif os.path.isdir(os.path.join(input_dir, video)):
-            gather_paths(os.path.join(input_dir, video), os.path.join(output_dir, video))
+            gather_paths(
+                os.path.join(input_dir, video), os.path.join(output_dir, video)
+            )
 
 
 def detect_shot(video_input, output_dir):
     os.makedirs(output_dir, exist_ok=True)
     video = os.path.basename(video_input)[:-4]
-    command = f"scenedetect --quiet -i {video_input} detect-adaptive --threshold 2 split-video --filename '{video}_shot_$SCENE_NUMBER' --output {output_dir}"
+    command = f"scenedetect --quiet -i {video_input} detect-adaptive --threshold 2 split-video- a \"-map 0:v:0 -map 0:a:0 -c copy\" --filename '{video}_shot_$SCENE_NUMBER' --output {output_dir}"
     # command = f"scenedetect --quiet -i {video_input} detect-adaptive --threshold 2 split-video --high-quality --filename '{video}_shot_$SCENE_NUMBER' --output {output_dir}"
     subprocess.run(command, shell=True)
 
@@ -50,7 +52,9 @@ def detect_shot_multiprocessing(input_dir, output_dir, num_workers):
 
     print(f"Detecting shot of {input_dir} ...")
     with Pool(num_workers) as pool:
-        for _ in tqdm.tqdm(pool.imap_unordered(multi_run_wrapper, paths), total=len(paths)):
+        for _ in tqdm.tqdm(
+            pool.imap_unordered(multi_run_wrapper, paths), total=len(paths)
+        ):
             pass
 
 
